c26dc9c044f06894b796bde9f9eac247c97e09d3
微冷 <1456227786@qq.com>
2020-02-13 14:30:45 +0800

    修复

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -18,7 +18,7 @@ from .base import *
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = '9qgh-%y9tr2*6cxvnzf8(u8a!&&&ea_-@-a18gooqunwozt)c$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
